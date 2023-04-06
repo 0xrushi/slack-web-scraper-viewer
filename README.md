1271c7fd514ef50c0f33e1f59719079d2a2d25aa
rushic24 <rushic24@gmail.com>
2023-04-05 19:42:46 -0500

    sc

--- README.md
+++ README.md
@@ -5,9 +5,7 @@ A simple just works viewer for https://github.com/iulspop/slack-web-scraper
 ## Setup 
 
 ```
-# copy the conversation.json in the project root directory
-
 pip install Flask==2.1.0
 
-python app.py
+python app.py /path/to/file.json
 ```
--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 import json
+import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open('conversation.json', 'r') as f:
+    with open(sys.argv[1], 'r') as f:
         conversation = json.load(f)
     return render_template('index.html', conversation=conversation)
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python app.py file.json")
+        exit(1)
     app.run()
